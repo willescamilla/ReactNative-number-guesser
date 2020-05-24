b5cdb738010ed3c104023adf2c1b0e440817fed0
William Escamilla <wescamilla10@gmail.com>
2020-05-23 23:44:45 -0400

    Set up Card view and button styles

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import Header from './components/Header';
+import StartGame from './screens/StartGame';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <Header title="Guess a Number"/>
+      <StartGame />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen: {
+    flex: 1
+  }
 });
